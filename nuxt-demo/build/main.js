6f3d8427b48d9843fd83576e7e00e35ea7812e7d
1130034920@qq.com <1130034920@qq.com>
2019-01-08 23:23:39 +0800

    Nuxt.js基础（3） vuex

--- nuxt-demo/build/main.js
+++ nuxt-demo/build/main.js
@@ -65,11 +65,18 @@ module.exports =
 /******/ 	__webpack_require__.p = "/";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 5);
+/******/ 	return __webpack_require__(__webpack_require__.s = 7);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(6);
+
+
+/***/ },
+/* 1 */
 /***/ function(module, exports) {
 
 module.exports = {
@@ -110,44 +117,91 @@ module.exports = {
 };
 
 /***/ },
-/* 1 */
+/* 2 */
 /***/ function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(4);
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_E_AmericanRegimen_nuxt_demo_node_modules_babel_runtime_regenerator__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_E_AmericanRegimen_nuxt_demo_node_modules_babel_runtime_regenerator___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_E_AmericanRegimen_nuxt_demo_node_modules_babel_runtime_regenerator__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_koa_router__ = __webpack_require__(5);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_koa_router___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_koa_router__);
 
 
-/***/ },
-/* 2 */
-/***/ function(module, exports) {
+var _this = this;
 
-module.exports = require("koa");
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+
+
+var router = new __WEBPACK_IMPORTED_MODULE_1_koa_router___default.a({
+  prefix: "/city"
+});
+
+router.get("/list", function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/__WEBPACK_IMPORTED_MODULE_0_E_AmericanRegimen_nuxt_demo_node_modules_babel_runtime_regenerator___default.a.mark(function _callee(ctx) {
+    return __WEBPACK_IMPORTED_MODULE_0_E_AmericanRegimen_nuxt_demo_node_modules_babel_runtime_regenerator___default.a.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            ctx.body = {
+              list: ["北京", "天津"]
+            };
+
+          case 1:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee, _this);
+  }));
+
+  return function (_x) {
+    return _ref.apply(this, arguments);
+  };
+}());
+
+/* harmony default export */ exports["a"] = router;
 
 /***/ },
 /* 3 */
 /***/ function(module, exports) {
 
-module.exports = require("nuxt");
+module.exports = require("koa");
 
 /***/ },
 /* 4 */
 /***/ function(module, exports) {
 
-module.exports = require("regenerator-runtime");
+module.exports = require("nuxt");
 
 /***/ },
 /* 5 */
+/***/ function(module, exports) {
+
+module.exports = require("koa-router");
+
+/***/ },
+/* 6 */
+/***/ function(module, exports) {
+
+module.exports = require("regenerator-runtime");
+
+/***/ },
+/* 7 */
 /***/ function(module, exports, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_E_AmericanRegimen_nuxt_demo_node_modules_babel_runtime_regenerator__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_E_AmericanRegimen_nuxt_demo_node_modules_babel_runtime_regenerator__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_E_AmericanRegimen_nuxt_demo_node_modules_babel_runtime_regenerator___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_E_AmericanRegimen_nuxt_demo_node_modules_babel_runtime_regenerator__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_koa__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_koa__ = __webpack_require__(3);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_koa___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_koa__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_nuxt__ = __webpack_require__(3);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_nuxt__ = __webpack_require__(4);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_nuxt___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_nuxt__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__interface_city__ = __webpack_require__(2);
 
 
+//引入路由
 var start = function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/__WEBPACK_IMPORTED_MODULE_0_E_AmericanRegimen_nuxt_demo_node_modules_babel_runtime_regenerator___default.a.mark(function _callee() {
     var app, host, port, config, nuxt, builder;
@@ -156,14 +210,14 @@ var start = function () {
         switch (_context.prev = _context.next) {
           case 0:
             app = new __WEBPACK_IMPORTED_MODULE_1_koa___default.a();
-            host = process.env.HOST || '127.0.0.1';
+            host = process.env.HOST || "127.0.0.1";
             port = process.env.PORT || 3000;
 
             // Import and Set Nuxt.js options
 
-            config = __webpack_require__(0);
+            config = __webpack_require__(1);
 
-            config.dev = !(app.env === 'production');
+            config.dev = !(app.env === "production");
 
             // Instantiate nuxt.js
             nuxt = new __WEBPACK_IMPORTED_MODULE_2_nuxt__["Nuxt"](config);
@@ -180,7 +234,8 @@ var start = function () {
             return builder.build();
 
           case 10:
-
+            // 使用路由
+            app.use(__WEBPACK_IMPORTED_MODULE_3__interface_city__["a" /* default */].routes()).use(__WEBPACK_IMPORTED_MODULE_3__interface_city__["a" /* default */].allowedMethods());
             app.use(function (ctx) {
               ctx.status = 200;
               ctx.respond = false; // Mark request as handled for Koa
@@ -189,10 +244,10 @@ var start = function () {
             });
 
             app.listen(port, host);
-            console.log('Server listening on ' + host + ':' + port); // eslint-disable-line no-console
+            console.log("Server listening on " + host + ":" + port); // eslint-disable-line no-console
 
-          case 13:
-          case 'end':
+          case 14:
+          case "end":
             return _context.stop();
         }
       }
@@ -205,6 +260,7 @@ var start = function () {
 }();
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
 
 
 
--- nuxt-demo/pages/search.vue
+++ nuxt-demo/pages/search.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="page">
     page is search
+    <ul>
+      <li
+        v-for="(item,idx) in list"
+        :key="idx"
+      >{{item}}</li>
+    </ul>
   </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
-  layout: 'search'
+  layout: 'search',
+  data() {
+    return {
+      list: ['广州']
+    }
+  },
+  async asyncData() {
+    let {status, data: {list}} = await axios.get('http://localhost:3000/city/list')
+    if (status === 200) {
+      return {list}
+    }
+  }
 }
 </script>
 <style scpped>
