b1c8cbed12007e3e4ba76fd5436d81dfcc102ffc
1130034920@qq.com <1130034920@qq.com>
2019-01-10 23:53:22 +0800

    7-2 首页Header开发-城市定位服务设计

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,9 +29,8 @@ module.exports = {
   css: [
     'element-ui/lib/theme-chalk/reset.css',
     'element-ui/lib/theme-chalk/index.css',
-    '~assets/css/main.css'
+    '@/assets/css/main.css'
   ],
-
   /*
   ** Plugins to load before mounting the App
   */
@@ -63,6 +62,6 @@ module.exports = {
     extend(config, ctx) {
       
     },
-    cache:true
+    cache:false
   }
 }
