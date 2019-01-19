705eeaecff5d7175fd0be67f2ca4a974c7252e9b
1130034920@qq.com <1130034920@qq.com>
2019-01-19 14:40:53 +0800

    登录，退出等接口

--- server/interface/users.js
+++ server/interface/users.js
@@ -11,7 +11,7 @@ let router = new Router({
 })
 
 let Store = new Redis().client
-
+// 注册
 router.post('/signup', async ctx => {
   const { username, password, email, code } = ctx.request.body
   if (code) {
@@ -77,3 +77,112 @@ router.post('/signup', async ctx => {
     }
   }
 })
+// 登录
+router.post('/signin', async ctx => {
+  return Passport.authenticate('local', function(err, user, info, status) {
+    if (err) {
+      ctx.body = {
+        code: -1,
+        msg: err
+      }
+    } else {
+      if (user) {
+        ctx.body = {
+          code: 0,
+          msg: '登录成功',
+          user
+        }
+        return ctx.login(user)
+      } else {
+        ctx.body = {
+          code: 1,
+          msg: info
+        }
+      }
+    }
+  })(ctx, next)
+})
+
+// 验证码
+router.post('/verify', async ctx => {
+  let username = ctx.request.body.username
+  const saveExpire = await Store.hget(`nodemail:${username}`, 'expire')
+  if (saveExpire && new Date().getTime() - saveExpire < 0) {
+    ctx.body = {
+      code: -1,
+      msg: '验证请求过于频繁，1分钟1次'
+    }
+    return false
+  }
+  let transporter = nodeMailer.createTransport({
+    host: Email.smtp.host,
+    port: 587,
+    secure: false,
+    auth: {
+      user: Email.smtp.user,
+      pass: Email.smtp.pass
+    }
+  })
+  let ko = {
+    code: Email.smtp.code(),
+    expire: Email.smtp.expire(),
+    email: ctx.request.body.email,
+    user: ctx.request.body.username
+  }
+  let mailOptions = {
+    from: `"认证邮件"<${Email.smtp.user}>`,
+    to: ko.email,
+    subject: '《高仿美团》注册码',
+    html: `你在《高仿美团》课程中注册，你的邀请码是${ko.code}`
+  }
+  //   发送
+  await transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log('error')
+    } else {
+      Store.hmset(
+        `nodemail:${ko.user}`,
+        'code',
+        ko.code,
+        'expire',
+        ko.expire,
+        'email',
+        ko.email
+      )
+    }
+  })
+  ctx.body = {
+    code: 0,
+    msg: '验证码已发送，可能有延时，有效期1分钟'
+  }
+})
+// 退出
+router.get('/exit', async ctx => {
+  await ctx.logout()
+  if (!ctx.isAuthenticated()) {
+    ctx.body = {
+      code: 0
+    }
+  } else {
+    ctx.body = {
+      code: -1
+    }
+  }
+})
+// 获取用户信息
+router.get('/getUser', async ctx => {
+  if (ctx.isAuthenticated()) {
+    const { username, email } = ctx.session.passport.user
+    ctx.body = {
+      user: username,
+      email
+    }
+  } else {
+    ctx.body = {
+      user: '',
+      email: ''
+    }
+  }
+})
+
+export default router
