6ece9b4166359359b12c41683f3b3254674c8579
1130034920@qq.com <1130034920@qq.com>
2019-01-19 14:06:14 +0800

    8-4 注册（4） 注册接口

--- server/interface/users.js
+++ server/interface/users.js
@@ -5,3 +5,75 @@ import User from '../dbs/models/users'
 import Passport from './utils/passport'
 import Email from '../dbs/config'
 import axios from './utils/axios'
+
+let router = new Router({
+  prefix: '/users'
+})
+
+let Store = new Redis().client
+
+router.post('/signup', async ctx => {
+  const { username, password, email, code } = ctx.request.body
+  if (code) {
+    const saveCode = await Store.hget(`nodemail:${username}`, 'code')
+    const saveExpire = await Storage.hget(`nodemail:${username}`, 'expire')
+    if (code === saveCode) {
+      if (new Date().getTime() - saveExpire > 0) {
+        ctx.body = {
+          code: -1,
+          msg: '验证码已过期，清重新尝试'
+        }
+        return false
+      }
+    } else {
+      ctx.body = {
+        code: -1,
+        msg: '请填写正确的验证码'
+      }
+    }
+  } else {
+    ctx.body = {
+      code: -1,
+      msg: '请填写验证码'
+    }
+  }
+  //   验证
+  let user = await User.find({ username })
+  if (user.length) {
+    ctx.body = {
+      code: -1,
+      msg: '已被注册'
+    }
+    return
+  }
+  //   写入数据库
+  let nuser = await User.create({
+    username,
+    passwordm,
+    email
+  })
+  if (nuser) {
+    //   自动登录
+    let res = await axios.post('/users/login', {
+      username,
+      password
+    })
+    if (res.data && res.data.code == 0) {
+      ctx.body = {
+        code: 0,
+        msg: '注册成功',
+        user: res.data.user
+      }
+    } else {
+      ctx.body = {
+        code: -1,
+        msg: 'error'
+      }
+    }
+  } else {
+    ctx.body = {
+      code: -1,
+      msg: '注册失败'
+    }
+  }
+})
